0eee23c3ebc06b3f25fc74d328c8943a0372bd7c
alfimiftanur <alfinurhakim1234@gmail.com>
2024-06-11 00:24:27 +0700

    update mysql

--- mysql/pw2024_tubes_233040013.sql
+++ mysql/pw2024_tubes_233040013.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: May 27, 2024 at 11:50 AM
+-- Generation Time: Jun 10, 2024 at 05:23 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -53,7 +53,36 @@ INSERT INTO `feedback` (`id`, `name`, `email`, `message`) VALUES
 (16, '', '', '12345'),
 (17, '', '', '1234'),
 (18, 'test', 'test@gmail.com', '1234'),
-(19, 'pepe', 'pepe@gmail.com', 'pepe pump');
+(19, 'pepe', 'pepe@gmail.com', 'pepe pump'),
+(20, 'bbbb', 'test@gmail.com', '123455'),
+(21, 'test', 'test@gmail.com', 'test');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `images`
+--
+
+CREATE TABLE `images` (
+  `id_images` int NOT NULL,
+  `image_path` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text,
+  `uploaded_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `id_role` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `images`
+--
+
+INSERT INTO `images` (`id_images`, `image_path`, `title`, `description`, `uploaded_at`, `id_role`) VALUES
+(61, '../uploads/bromo.jpg', 'Bromo', 'bromo', '2024-06-09 13:43:53', 1),
+(62, '../uploads/flores.jpeg', 'Flores', 'flores', '2024-06-09 13:58:54', 1),
+(63, '../uploads/Pantai Pangandaran - Indonesia.jpeg', 'Pangandaran', 'pnd', '2024-06-09 14:08:23', 1),
+(65, '../uploads/bali.jpg', 'Bali', 'bali', '2024-06-10 05:46:00', 1),
+(66, '../uploads/jakarta.jpg', 'Jakarta', 'jakarta', '2024-06-10 05:52:03', 1),
+(67, '../uploads/bandung.jpg', 'Bandung', 'bandung', '2024-06-10 05:52:29', 1);
 
 -- --------------------------------------------------------
 
@@ -94,14 +123,36 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `gambar`, `username`, `email`, `password`, `id_role`) VALUES
-(1, '../uploads/admin-icon.png', 'alfi', 'alfi@gmail.com', '$2y$10$Va/Z9YCUFgjGm.JtUefiMOj7oNzj/GADJotAhcJ9MKwCStSgH5x5a', 1),
 (2, '../uploads/admin-icon.png', 'admin', 'admin@gmail.com', '$2y$10$lMAL5Zfmf1qlSzi4n0GNGOIEGW0HODa0OlaPFhRbjwFwxa28YvPza', 1),
-(42, '../uploads/test-profile.jpg', 'nana', 'nana@gmail.com', '$2y$10$jcTsPXEt0IKGMQ/Sjqf4aOB0Hu8M5n5Q3TU5sab76jXAbZWnBq2Yi', 2),
-(43, '../uploads/test-profile.jpg', 'masum', 'masum@gmail.com', '$2y$10$3WckOsw8a60MYNJuCPF.ueoIOj3Sa9TTUVgFpkvYr.T6OHjmlJ6AW', 2),
-(45, '../uploads/test-profile.jpg', 'maman', 'maman@gmail.com', '$2y$10$w96cl6pA1LL9rkgUCmNdK.PxT239j05BiUOe2zL5R7AX4uMbrgmmm', 2),
-(55, '../uploads/sad-pepe.jpg', 'sad pepe', 'pepe@gmail.com', '$2y$10$qar4dtfUe720s5S..YKXTOrx0gxvt8/B7DiNxmCNh2gs56kfXS29S', 2),
-(59, '../uploads/test-profile.jpg', 'andy', 'andy@gmail.com', '$2y$10$Y207VvnhvS4Kj/yU0a0Ege9bU63df.J16JiEkLkUsfv3S67vIjxV.', 2),
-(60, '../uploads/test-image.jpg', 'rio', 'rio@gmail.com', '$2y$10$n0vpXTmLJnwzCCq1asD6zePQ/.dcWoOhFjJgdkBFmBdCBnpK52vzq', 2);
+(96, '../uploads/admin-icon.png', 'alfi', 'alfi@gmail.com', '$2y$10$DNFFL1.2CbmmHVfTGmeWcetmNd2YwDJmEvOXMVReZ1OaqYuY/0Laa', 1),
+(100, '../uploads/𝒕𝒉𝒆 𝒘𝒊𝒏𝒅 𝒓𝒊𝒔𝒆𝒔.jpeg', 'test', 'test@gmail.com', '$2y$10$fR1rh9LQ0.fZT0aDmq2fxeV8YpY4jadsiH/OP6SJL9fUwujXzfDMa', 2),
+(102, '../assets/img/default.jpg', 'mamat', 'mamat@gmail.com', '$2y$10$D1RDudMY4c32ax/fcluymeNcAdw4axUBGDfVGdkgP1MVqmd0UylIS', 2),
+(103, '../assets/img/default.jpg', 'mifta', 'mifta@gmail.com', '$2y$10$2CRybEDCAuf/TPd.CoPe/eYT0V0zJpm5UeQ3udOyEfMjX8CBrvNyG', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_sessions`
+--
+
+CREATE TABLE `user_sessions` (
+  `id_sessions` int NOT NULL,
+  `id` int DEFAULT NULL,
+  `session_start` datetime DEFAULT NULL,
+  `session_end` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `user_sessions`
+--
+
+INSERT INTO `user_sessions` (`id_sessions`, `id`, `session_start`, `session_end`) VALUES
+(15, 96, '2024-06-09 20:16:01', '2024-06-10 15:31:40'),
+(18, 100, '2024-06-10 09:25:54', '2024-06-10 09:30:04'),
+(19, 102, '2024-06-10 09:30:12', NULL),
+(20, 2, '2024-06-10 15:31:56', '2024-06-10 23:55:46'),
+(21, 103, '2024-06-10 21:22:03', '2024-06-10 21:22:12'),
+(22, 96, '2024-06-10 23:58:24', NULL);
 
 --
 -- Indexes for dumped tables
@@ -112,6 +163,13 @@ INSERT INTO `user` (`id`, `gambar`, `username`, `email`, `password`, `id_role`) 
 --
 ALTER TABLE `feedback`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `images`
+--
+ALTER TABLE `images`
+  ADD PRIMARY KEY (`id_images`),
+  ADD KEY `images_ibfk_1` (`id_role`);
 
 --
 -- Indexes for table `role`
@@ -127,6 +185,13 @@ ALTER TABLE `user`
   ADD KEY `id_role` (`id_role`);
 
 --
+-- Indexes for table `user_sessions`
+--
+ALTER TABLE `user_sessions`
+  ADD PRIMARY KEY (`id_sessions`),
+  ADD KEY `fk_user_sessions_user_id` (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -134,23 +199,48 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `feedback`
 --
 ALTER TABLE `feedback`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `images`
+--
+ALTER TABLE `images`
+  MODIFY `id_images` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=104;
+
+--
+-- AUTO_INCREMENT for table `user_sessions`
+--
+ALTER TABLE `user_sessions`
+  MODIFY `id_sessions` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- Constraints for dumped tables
 --
 
 --
+-- Constraints for table `images`
+--
+ALTER TABLE `images`
+  ADD CONSTRAINT `images_ibfk_1` FOREIGN KEY (`id_role`) REFERENCES `role` (`id_role`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
 -- Constraints for table `user`
 --
 ALTER TABLE `user`
   ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`id_role`) REFERENCES `role` (`id_role`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `user_sessions`
+--
+ALTER TABLE `user_sessions`
+  ADD CONSTRAINT `fk_user_sessions_user_id` FOREIGN KEY (`id`) REFERENCES `user` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `user_sessions_ibfk_1` FOREIGN KEY (`id`) REFERENCES `user` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
